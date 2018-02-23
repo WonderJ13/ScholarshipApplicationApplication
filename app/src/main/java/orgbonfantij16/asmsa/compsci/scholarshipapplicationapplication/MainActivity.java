34c6b9fa300af45bf688c1fbe503a44675c34a1c
WonderJ13 <wonderj013@yahoo.com>
2018-02-23 06:44:59 -0600

    Initial Commit

---
+++ app/src/main/java/orgbonfantij16/asmsa/compsci/scholarshipapplicationapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package orgbonfantij16.asmsa.compsci.scholarshipapplicationapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
